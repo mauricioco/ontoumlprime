4664d10fb4e12343b9177a9116acbb8263a3474f
Mauricio Carvalho de Oliveira <mauricio.carvoliveira@gmail.com>
2016-02-07 02:59:09 -0200

    Added the changes missing from last commit. DAMN YOU SHIFT+ENTER!!
    Now there's a log class to make logging easier, even though it's very
    simple.

---
+++ br.ufes.inf.nemo.ontouml.ontoumlprime.visualizer/src/br/ufes/inf/nemo/ontouml/ontoumlprime/visualizer/log/Log.java
@@ -0,0 +1,46 @@
+package br.ufes.inf.nemo.ontouml.ontoumlprime.visualizer.log;
+
+public class Log {
+	
+	//TODO make class name stand out.
+	
+	private static int currentLevel = 1000000;
+	
+	public static void setLogLevel(int level) {
+		currentLevel = level;
+	}
+	
+	public static void p(int level, Class<?> sender, String message) {
+		printIdentation(level);
+		if(level <= currentLevel) {
+			System.out.println("(" + level + ") " + sender.getSimpleName() + ": " + message);
+		}
+	}
+	
+	public static void e(int level, Class<?> sender, String message) {
+		printIdentationErr(level);
+		if(level <= currentLevel) {
+			System.err.println("(" + level + ") " + sender.getSimpleName() + ": " + message);
+		}
+	}
+	
+	public static void e(int level, Class<?> sender, Exception exception) {
+		printIdentationErr(level);
+		if(level <= currentLevel) {
+			System.err.println("(" + level + ") " + sender.getSimpleName() + ": " + exception.getLocalizedMessage());
+		}
+	}
+	
+	private static void printIdentation(int level) {
+		for(int i=100; i<level; i+=100) {
+			System.out.print(" ");
+		}
+	}
+	
+	private static void printIdentationErr(int level) {
+		for(int i=100; i<level; i+=100) {
+			System.err.print(" ");
+		}
+	}
+	
+}
